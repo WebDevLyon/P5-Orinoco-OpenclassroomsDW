cd3deaf4045bf75b559369135c941e82c9011529
Nicolas Lachise <webdeveloppeurlyon@gmail.com>
2020-04-22 17:43:53 +0200

    responsive page confirmation - js retour api après commande

--- js/products.js
+++ js/products.js
@@ -414,3 +414,22 @@ addition = () =>{
   };
 });
   };
+
+/*Affichage des informations sur la page de confirmation
+**********************************************/
+resultOrder = () =>{
+  if(sessionStorage.getItem("order") != null){
+    //Parse du session storage
+    let order = JSON.parse(sessionStorage.getItem("order"));
+    //Implatation de prénom et de id de commande dans le html sur la page de confirmation
+    document.getElementById("lastName").innerHTML = order.contact.lastName
+    document.getElementById("orderId").innerHTML = order.orderId
+    
+    //Suppression de la clé du sessionStorage pour renvoyer au else si actualisation de la page ou via url direct
+    sessionStorage.removeItem("order");
+  }else{
+  //avertissement et redirection vers l'accueil
+  alert("Aucune commande passée, vous êtes arrivé ici par erreur");
+  window.open("./index.html");
+}
+}
